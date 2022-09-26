211f533180807fc51f6da52ced20136dd0113741
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-09-26 19:26:54 -0400

    added javascript for the modal

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,22 @@
 var drinks = ["Coffee", "Tea", ""]
 var sizes = ["small", "medium", "large"]
 
+var modal= document.getElementById("cardModal");
+
+var btn = document.getElementById("mdlBtn")
+
+var span = decument.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+modal.style.display ="block"
+}
+
+span.onclic;k = function() {
+modal.style.display="none"
+}
+
+window.onclick = function(event) {
+if (event.target == modal){
+    modal.style.display = "none";
+}
+}
