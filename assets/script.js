8b51e227ebe558624d25ab4ba52475c80ec59c7f
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-09-26 20:27:59 -0400

    changing task

--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,50 @@
 var drinks = ["Coffee", "Tea", ""]
 var sizes = ["small", "medium", "large"]
 
-var modal= document.getElementById("myModal");
+var menuEl = document.querySelector("#menu")
+var addBtns = document.querySelector(".addBtn")
+var cart = []
+//sets the object equal to one which has been previously saved in local storage if there is one
+var savedCart = JSON.parse(localStorage.getItem("cart"));
+if (savedCart !== null) {
+    cart = savedCart;
+}
+console.log(cart)
 
-var btn = document.getElementById("myBtn")
+function getMenu() {
+fetch('https://api.sampleapis.com/coffee/hot')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    displayMenu(data)
+  });
+}
 
-var span = document.getElementsByClassName("close")[0];
+function displayMenu(data) {
+    for (i = 0; i < 10; i++) {
+        var menuItem = document.createElement("div")
+        var menuImg = document.createElement("img")
+        var menuHeading = document.createElement("h4")
+        var menuP = document.createElement("p")
+        var menuBtn = document.createElement("button")
+        menuItem.setAttribute("class", "MenuItem")
+        menuImg.setAttribute("src", data[i].image)
+        menuHeading.textContent = data[i].title
+        menuP.textContent = data[i].description
+        menuBtn.setAttribute("class", "addBtn")
+        menuBtn.textContent = "Add to Cart"
+        menuEl.appendChild(menuItem)
+        menuItem.appendChild(menuImg)
+        menuItem.appendChild(menuHeading)
+        menuItem.appendChild(menuP)
+        menuItem.appendChild(menuBtn)
+    }
+}
 
+
+getMenu();
+
+//allow users to add items to their car
+//show what they have in their cart
