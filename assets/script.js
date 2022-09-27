894061e70e61bc9b264c6f7687d8ae57a665851c
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-09-26 20:09:44 -0400

    working on it

--- assets/script.js
+++ assets/script.js
@@ -5,22 +5,9 @@
 var drinks = ["Coffee", "Tea", ""]
 var sizes = ["small", "medium", "large"]
 
-var modal= document.getElementById("cardModal");
+var modal= document.getElementById("myModal");
 
-var btn = document.getElementById("mdlBtn")
+var btn = document.getElementById("myBtn")
 
-var span = decument.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function () {
-modal.style.display ="block"
-}
-
-span.onclic;k = function() {
-modal.style.display="none"
-}
-
-window.onclick = function(event) {
-if (event.target == modal){
-    modal.style.display = "none";
-}
-}
